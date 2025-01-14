f56ab4ba0f342861350ac976513fcc19052eb355
sushant09112004 <jadhavsushant379@gmail.com>
2025-01-15 00:31:13 +0530

    basic folder structure

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        abiut us page 
+        addd the foler under app directory and and give the name as localhost3000/folder name 
+        eg:localhost3000/about
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <h1>Sanket's Adsare</h1>
+        <p>Welcome to Sanket's Adsare!</p>
+        <p>This is a simple Next.js application with Tailwind CSS and Geist UI.</p>
+      <a href="https://nextjs.org"></a>
+    </div>
+  )
+}
+
+export default page
